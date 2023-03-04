16966179158c05496ad960644ac248fe31e1c38b
Iamfrans <1323544@senati.pe>
2023-03-04 07:42:53 -0500

    Sin comentarios x3

--- +5 puntos.py
+++ +5 puntos.py
@@ -4,3 +4,5 @@ while x! =0:
         n.append(x)
         x =int(input("vuelva a ingresar un numero: "
         x = int(input("si no desea ingresar un numro ingrese el val 0: "
+print("Lista : ", (n))
+print("Tupa : ", (n))
