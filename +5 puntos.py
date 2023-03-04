a831bff272d38b5a461792c4e080ebbd06d659ed
Iamfrans <1323544@senati.pe>
2023-03-04 07:40:49 -0500

    Sin comentarios x2

--- +5 puntos.py
+++ +5 puntos.py
@@ -1,3 +1,6 @@
 n = []
 x = int(input("Introduzca un numero: ")
-        
+while x! =0:
+        n.append(x)
+        x =int(input("vuelva a ingresar un numero: "
+        x = int(input("si no desea ingresar un numro ingrese el val 0: "
